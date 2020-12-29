a8effea0e189966e93f4030874975c7ff09c7031
Ben Cordero <bencord0@condi.me>
2020-12-29 15:43:45 +0000

    rust: Use crossbeam instead of mpsc

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
-use std::{
-    sync::mpsc::{
-        channel,
-        Receiver,
-    },
-    thread,
-};
+use crossbeam::channel::{self, Receiver};
+use std::thread;
 
 fn arrange_data(from: i64, to: i64) -> Receiver<i64> {
-    let (sender, receiver) = channel();
+    let (sender, receiver) = channel::unbounded();
     let increment = if from > to { -1 } else { 1 };
 
     thread::spawn(move || {
@@ -25,9 +20,13 @@ fn arrange_data(from: i64, to: i64) -> Receiver<i64> {
 }
 
 fn merge(channels: Vec<Receiver<i64>>) -> Receiver<i64> {
-    let (sender, receiver) = channel();
+    let (sender, receiver) = channel::unbounded();
+
     for channel in channels {
+        // Each thread needs it's own copy of the sender
         let sender = sender.clone();
+
+        // move: channel and sender need to be cloned for each thread
         thread::spawn(move || {
             for value in channel {
                 if let Err(unsent_value) = sender.send(value) {
@@ -45,6 +44,8 @@ fn main() {
     let c2 = arrange_data(7, -3);
     let c3 = arrange_data(100, 110);
 
+    // We won't use the channels in `main()` anymore, so it is safe
+    // to move their ownership to `merge` to avoid a copy/clone.
     for value in merge(vec![c1, c2, c3]) {
         println!("{}", value);
     }
